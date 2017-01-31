193a465b962c662002c63cc0fd2a8807200b7110
Hyeonsu Lee <incleaf@gmail.com>
2017-01-31 21:14:57 +0900

    Add example of redis pub/sub pattern

--- socket.js
+++ socket.js
@@ -2,11 +2,22 @@ const redis = require('redis');
 const sub = redis.createClient();
 const pub = redis.createClient();
 
+sub.on('message', (channel, message) => {
+  console.log('i\'ve got message!!!!', channel, message);
+});
+
+sub.subscribe('chat', function(channel, message) {
+  console.log('subscribing...');
+  // connection.emit(channel, message);
+});
+
 function connectionHandler(connection) {
-  connection.emit('message', { payload: 'world' });
-  connection.on('chat', function (data) {
-    console.log(data);
+  connection.on('chat', function(data) {
+    // console.log(data);
+    pub.publish('chat', JSON.stringify(data));
   });
+
+
 }
 
 module.exports = connectionHandler;
